e02a8decae41b47248617b71dc92ec5d89b84b58
wuci1006 <wuci1006@example.com>
2020-01-20 14:12:20 +0800

    update home module

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,6 +1,12 @@
 <template>
-    <div class="hello">
-        <h1>{{ msg }}</h1>
+    <div class="home">
+		<div class="home_content">
+			<h2>模块</h2>
+			<div>
+				<h3>子模块1</h3>
+				<p>关于子模块的介绍1</p>
+			</div>
+		</div>
     </div>
 </template>
 
@@ -8,12 +14,42 @@
     export default {
         data() {
             return {
-                msg: 'home'
+				// 轮播图模块,各种菜案模块,选项卡模块
+				moduleArr:[
+					{
+						id:1,
+						title:'图形管理',
+						child:[
+							{id:11,title:'饼图',desc:''},
+							{id:11,title:'折线图',desc:''},
+							{id:11,title:'柱状图',desc:''}
+						],
+					},
+				]
             }
         }
     }
 </script>
 
 <style scoped>
+	.home{
+		margin:20px;
+		background-color: #fff;
+	}
+	.home_content{
+		padding:15px;
+	}
+	.home_content h2{
+		padding:10px 0;
+	}
+	.home_content h3{
+		padding:10px 0;
+	}
+	.home_content p{
+		line-height: 25px;
+		padding:10px 0;
+		font-size:14px;
+		color:#515a6e;
+	}
 
 </style>
